949d1c0e997339d26ed780cbaac40caa83cf5db9
kostet1994 <kostet1994@rambler.ru>
2018-03-25 01:19:13 +0300

    initial

---
+++ app/src/main/java/com/vkdisk/konstantin/vkdisk_mobile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vkdisk.konstantin.vkdisk_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
