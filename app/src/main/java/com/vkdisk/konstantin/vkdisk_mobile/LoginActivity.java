4c284a150c5dc1f0f75effa4b66bd311159d9acd
kostet1994 <kostet1994@rambler.ru>
2018-04-05 20:16:53 +0300

    Use preferences to save cookie

--- app/src/main/java/com/vkdisk/konstantin/vkdisk_mobile/LoginActivity.java
+++ app/src/main/java/com/vkdisk/konstantin/vkdisk_mobile/LoginActivity.java
@@ -2,6 +2,7 @@ package com.vkdisk.konstantin.vkdisk_mobile;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
@@ -16,12 +17,9 @@ import android.widget.Toast;
 import com.vkdisk.konstantin.vkdisk_mobile.retrofit.DocumentApi;
 import com.vkdisk.konstantin.vkdisk_mobile.retrofit.SessionApi;
 
-import java.io.IOException;
 import java.net.URI;
 
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.ResponseBody;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
@@ -33,11 +31,15 @@ public class LoginActivity extends AppCompatActivity {
 
     public final String LOG_TAG = this.getClass().getSimpleName();
 
+
     private WebView webView;
     private String loginUrl;
-    private String cookie;
     private String cookies;
     private String redirectUrl;
+    private String csrf;
+    private String session;
+    private String store;
+    private String cookie_key;
 
     public static Intent createAuthActivityIntent(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
@@ -53,6 +55,8 @@ public class LoginActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         loginUrl = getString(R.string.auth_url);
         redirectUrl = getString(R.string.redirect_url);
+        store = getString(R.string.cookie_store);
+        cookie_key = getString(R.string.cookie);
     }
 
     @Override
@@ -73,19 +77,19 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         public boolean shouldOverrideUrlLoading(WebView view, String url){
-            Log.d(LOG_TAG, url);
 
             if(url.startsWith(redirectUrl)){
                 url.indexOf("#");
                 String substring = url.substring(url.indexOf("#")+1, url.length());
                 String[] urls = substring.split("&");
                 final String code = urls[0].split("=")[1];
-                String state = urls[1].split("=")[1];
+                final String state = urls[1].split("=")[1];
                 HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
                 interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
                 OkHttpClient client = new OkHttpClient.Builder()
 //                        .addInterceptor(interceptor)
                         .addNetworkInterceptor(interceptor)
+                        .followRedirects(false)
                         .build();
                 Retrofit retrofit = new Retrofit.Builder()
                         .baseUrl(webView.getResources().getString(R.string.basic_url))
@@ -95,11 +99,12 @@ public class LoginActivity extends AppCompatActivity {
                 getSession.getSession(code, state, cookies).enqueue(new Callback<ResponseBody>() {
                     @Override
                     public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        Log.d(LOG_TAG, response.headers().toString());
-                        cookie = response.headers().toMultimap().get("Set-Cookie").get(0);
-                        cookies += "; " + cookie.substring(0, cookie.indexOf(";"));
-                        //cookies = cookie.substring(0, cookie.indexOf(";"));
+                        session = response.headers().toMultimap().get("Set-Cookie").get(0);
+                        csrf = response.headers().toMultimap().get("Set-Cookie").get(1);
+                        cookies += "; " + session.substring(0, session.indexOf(";"))  + "; " + csrf.substring(0, csrf.indexOf(";"));
                         Log.d(LOG_TAG, cookies);
+                        SharedPreferences pref = getSharedPreferences(store, MODE_PRIVATE);
+                        pref.edit().putString(cookie_key, cookies).apply();
                         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
                         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
                         OkHttpClient client = new OkHttpClient.Builder()
@@ -111,7 +116,7 @@ public class LoginActivity extends AppCompatActivity {
                                 .client(client)
                                 .build();
                         DocumentApi documentApi = retrofit.create(DocumentApi.class);
-                        documentApi.getAllDocument(cookies, cookie.substring(cookie.indexOf("=") + 1, cookie.indexOf(";"))).enqueue(new Callback<ResponseBody>() {
+                        documentApi.getAllDocument(cookies, csrf.substring(csrf.indexOf("=") + 1, csrf.indexOf(";"))).enqueue(new Callback<ResponseBody>() {
                             @Override
                             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                                 Log.d(LOG_TAG, String.valueOf(response.code()));
--- app/src/main/java/com/vkdisk/konstantin/vkdisk_mobile/SplashActivity.java
+++ app/src/main/java/com/vkdisk/konstantin/vkdisk_mobile/SplashActivity.java
@@ -1,16 +1,27 @@
 package com.vkdisk.konstantin.vkdisk_mobile;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private String store;
+    private String cookie_key;
+    private Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
+        store = getString(R.string.cookie_store);
+        cookie_key = getString(R.string.cookie);
+        SharedPreferences pref = getSharedPreferences(store, MODE_PRIVATE);
+        if(pref.contains(cookie_key)){
+            intent = new Intent(this, ListActivity.class);
+        }else {
+            intent = new Intent(this, MainActivity.class);
+        }
         startActivity(intent);
         finish();
     }
